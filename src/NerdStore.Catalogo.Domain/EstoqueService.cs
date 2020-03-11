a55101dc47d2225e40f56388c6625f570bf0c2ac
srlacerda <diego.lacerda.alves@gmail.com>
2020-03-11 20:20:12 -0300

    Eventos de Domínio

--- src/NerdStore.Catalogo.Domain/EstoqueService.cs
+++ src/NerdStore.Catalogo.Domain/EstoqueService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NerdStore.Catalogo.Domain.Events;
+using NerdStore.Core.Bus;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +10,10 @@ namespace NerdStore.Catalogo.Domain
     public class EstoqueService : IEstoqueService
     {
         private readonly IProdutoRepository _produtoRepository;
-        //private readonly IMediatrHandler _bus;
+        private readonly IMediatrHandler _bus;
 
-        public EstoqueService(IProdutoRepository produtoRepository)
-                              //IMediatrHandler bus)
+        public EstoqueService(IProdutoRepository produtoRepository,
+                              IMediatrHandler bus)
         {
             _produtoRepository = produtoRepository;
            // _bus = bus;
@@ -27,10 +29,10 @@ namespace NerdStore.Catalogo.Domain
             produto.DebitarEstoque(quantidade);
 
             // TODO: Parametrizar a quantidade de estoque baixo
-            //if (produto.QuantidadeEstoque < 10)
-            //{
-            //    await _bus.PublicarEvento(new ProdutoAbaixoEstoqueEvent(produto.Id, produto.QuantidadeEstoque));
-            //}
+            if (produto.QuantidadeEstoque < 10)
+            {
+                await _bus.PublicarEvento(new ProdutoAbaixoEstoqueEvent(produto.Id, produto.QuantidadeEstoque));
+            }
 
             _produtoRepository.Atualizar(produto);
             return await _produtoRepository.UnitOfWork.Commit();
---
+++ src/NerdStore.Core/Bus/MediatrHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using NerdStore.Core.Messages;
+
+namespace NerdStore.Core.Bus
+{
+    public class MediatrHandler : IMediatrHandler
+    {
+        private readonly IMediator _mediator;
+        public MediatrHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public async Task PublicarEvento<T>(T evento) where T : Event
+        {
+            await _mediator.Publish(evento);
+        }
+    }
+}
