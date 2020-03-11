884f7abdd2c76f15c06f56048be15865840bd0d8
srlacerda <diego.lacerda.alves@gmail.com>
2020-03-11 19:18:39 -0300

    Repositórios

--- src/NerdStore.Catalogo.Domain/Categoria.cs
+++ src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -9,6 +9,13 @@ namespace NerdStore.Catalogo.Domain
     {
         public string Nome { get; private set; }
         public int Codigo { get; private set; }
+
+        //EF Relation - Relacionametno do EntityFramework
+        public ICollection<Produto> Produtos { get; set; }
+        protected Categoria()
+        {
+
+        }
         public Categoria(string nome, int codigo)
         {
             Nome = nome;
