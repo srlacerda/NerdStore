312ec7f6a891462819c2497025fdf80110070d90
srlacerda <diego.lacerda.alves@gmail.com>
2020-03-12 14:54:31 -0300

    Conectando a Camada de Aplicação

--- src/NerdStore.Catalogo.Domain/Produto.cs
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -18,6 +18,8 @@ namespace NerdStore.Catalogo.Domain
         public Categoria Categoria { get; private set; }
         public Dimensoes Dimensoes { get; set; }
 
+        protected Produto(){}
+
         public Produto(string nome, string descricao, bool ativo, decimal valor, Guid categoriaId, DateTime dataCadastro, string imagem, Dimensoes dimensoes)
         {
             CategoriaId = categoriaId;
---
+++ src/NerdStore.WebApp.MVC/Setup/DependenyInjection.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using NerdStore.Catalogo.Application.Services;
+using NerdStore.Catalogo.Data;
+using NerdStore.Catalogo.Data.Repository;
+using NerdStore.Catalogo.Domain;
+using NerdStore.Catalogo.Domain.Events;
+using NerdStore.Core.Bus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NerdStore.WebApp.MVC.Setup
+{
+    public static class DependenyInjection
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            // Domain Bus (Mediator)
+            services.AddScoped<IMediatrHandler, MediatrHandler>();
+
+            // Catalogo
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IProdutoAppService, ProdutoAppService>();
+            services.AddScoped<IEstoqueService, EstoqueService>();
+            services.AddScoped<CatalogoContext>();
+
+            services.AddScoped<INotificationHandler<ProdutoAbaixoEstoqueEvent>, ProdutoEventHandler>();
+        }
+    }
+}
