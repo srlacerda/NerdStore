91af909bdeaf6d578b9e368ffc4fadf1b1741cf8
srlacerda <diego.lacerda.alves@gmail.com>
2020-03-13 20:36:52 -0300

    Setup BC Vendas - CQRS

---
+++ src/NerdStore.Vendas.Domain/Pedido.cs
@@ -0,0 +1,22 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class Pedido : Entity, IAggregateRoot
+    {
+        public int Codigo { get; private set; }
+        public Guid ClienteId { get; private set; }
+        public Guid? VoucherId { get; private set; }
+        public bool VoucherUtilizado { get; private set; }
+        public decimal Desconto { get; private set; }
+        public decimal ValorTotal { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public PedidoStatus PedidoStatus { get; private set; }
+
+        private readonly List<PedidoItem> _pedidoItems;
+        public IReadOnlyCollection<PedidoItem> PedidoItems => _pedidoItems;
+    }
+}
