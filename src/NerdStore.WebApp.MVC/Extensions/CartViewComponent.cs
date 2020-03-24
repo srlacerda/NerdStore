b06aec8a1e92db4d6e70f1468e587a93a01df34a
srlacerda <diego.lacerda.alves@gmail.com>
2020-03-24 16:08:54 -0300

    CQRS - Implementando Queries

---
+++ src/NerdStore.WebApp.MVC/Extensions/CartViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NerdStore.Vendas.Application.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NerdStore.WebApp.MVC.Extensions
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly IPedidoQueries _pedidoQueries;
+
+        // TODO: Obter cliente logado
+        //protected Guid ClienteId = Guid.Parse("4885e451-b0e4-4490-b959-04fabc806d32");
+        protected Guid ClienteId = Guid.Parse("cb878264-cae9-4f1f-9871-3134e94fe49e");
+
+        public CartViewComponent(IPedidoQueries pedidoQueries)
+        {
+            _pedidoQueries = pedidoQueries;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var carrinho = await _pedidoQueries.ObterCarrinhoCliente(ClienteId);
+            var itens = carrinho?.Items.Count ?? 0;
+
+            return View(itens);
+        }
+    }
+}
