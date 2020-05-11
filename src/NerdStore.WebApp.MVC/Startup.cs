984b21ecb3fc8ec27b7c737e441cc4bad84e3cd7
srlacerda <diego.lacerda.alves@gmail.com>
2020-05-11 15:29:00 -0300

    commit

--- src/NerdStore.WebApp.MVC/Startup.cs
+++ src/NerdStore.WebApp.MVC/Startup.cs
@@ -65,6 +65,7 @@ namespace NerdStore.WebApp.MVC
             services.AddMediatR(typeof(Startup));
 
             services.RegisterServices();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
